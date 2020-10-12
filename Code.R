af38904f5cf61a08f74fc4abb96a0b8cc0d84b90
LindaBetz <linda.betz@uk-koeln.de>
2020-10-12 18:49:58 +0200

    Added ML-case

--- Code.R
+++ Code.R
@@ -3,9 +3,6 @@
 # Dann jeweils die erste Komponente mit einander korrelieren und die Residuen ausgeben.
 # Für 1) input: matrix 1 & matrix 2, output: residuen
 
-set.seed(1)
-test_1 <- matrix(rnorm(1500), nrow = 150)
-test_2 <- matrix(rnorm(1500), nrow = 150)
 
 GetResidualsPCA <- function(input_risk, input_functioning) {
   # some initial checks
@@ -19,21 +16,81 @@ GetResidualsPCA <- function(input_risk, input_functioning) {
   if (nrow(input_risk) < ncol(input_risk))
     message("There are fewier observations than variables. Results might be unstable.")
   
-# compute PCA separately for risk and functioning variables
+  # compute PCA separately for risk and functioning variables
   pca_risk <-
     prcomp(input_risk, scale = TRUE)$x[, 1] # extract first component
   pca_functioning <-
     prcomp(input_functioning, scale = TRUE)$x[, 1] # extract first component
   
-  # linear model: predict functioning by risk 
-  lm_resilience <- lm(pca_risk ~ pca_functioning) 
+  # linear model: predict functioning by risk
+  lm_resilience <- lm(pca_risk ~ pca_functioning)
   
   # extract & return residuals of this linear model
-  return(data.frame(residuals = lm_resilience$residuals)) 
+  return(data.frame(residuals = lm_resilience$residuals))
   
 }
 
-GetResidualsPCA(test_1, test_2)
+# some test data
+set.seed(1)
+test_1 <- matrix(rnorm(1500), nrow = 150)
+test_2 <- matrix(rnorm(1500), nrow = 150)
 
+GetResidualsPCA(test_1, test_2)
+# works
+
+
+library(dplyr)
 # Für 2) input: task & mlr learner, output: residuen
 # 2) Mittels ML: ein ML Model aufstellen in welchem durch Prädiktoren (z.B. Env. Risk Faktoren) eine Variable (Functioning) prädiziert wird, dann die Residuen ausgeben.
+
+GetResidualsML <- function(task, learner, cv_folds, cv_reps) {
+  # some initial checks
+  if (learner$type != "regr")
+    stop("Error: Learner type must be 'regression'.")
+  if (learner$predict.type != "response")
+    stop("Error: Predict type must be 'response'.")
+  if (sum(task$task.desc$n.feat) > task$task.desc$size)
+    warning("Task with more features than observations given. Results might be unstable")
+  
+  # define resampling scheme
+  if (cv_folds <= 1) {
+    warning("Number of folds must be at least 2 for CV. Setting cv_folds to 5.")
+    cv_folds <- 5
+  }
+  if (cv_reps > 1) {
+    rdesc <-
+      mlr::makeResampleDesc("RepCV", folds = cv_folds, reps = cv_reps)
+  } else {
+    rdesc <- mlr::makeResampleDesc("CV", iters = cv_folds)
+  }
+  
+  resampled_models <-
+    mlr::resample(learner, task, rdesc, models = TRUE, show.info = FALSE)
+  
+  predictions <- getRRPredictions(resampled_models)$data
+  
+  residuals <-
+    predictions %>% mutate(residuals = truth - response) %>%
+    group_by(id) %>%
+    summarize(residuals = mean(residuals))
+  
+  return(residuals)
+  
+}
+
+# some test data
+data(BostonHousing, package = "mlbench")
+regr.lrn <-
+  mlr::makeLearner("regr.gbm", par.vals = list(n.trees = 1000, interaction.depth = 5))
+regr.task <-
+  mlr::makeRegrTask(id = "bh", data = BostonHousing, target = "medv")
+task <- regr.task
+learner <- regr.lrn
+
+
+GetResidualsML(
+  task = regr.task,
+  learner = regr.lrn,
+  cv_folds = 2,
+  cv_reps = 0
+)
